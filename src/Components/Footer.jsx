c38232a10d4f38307d80202c745c7144339cf53b
Kb-Jr <abdulkabiruj@gmail.com>
2023-10-23 19:37:38 +0100

    Created and added a footer component
    Footer component added to the General layout component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='fixed w-full flex items-center justify-center bottom-0 bg-secondary-dark-bg text-white rounded-b-2xl mt-2'>
+            <p className='p-3'>2022 #VanLife</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
+import Footer from './footer'
 
 
 const Layout = () => {
   return (
-    <div className='m-5 p-5 flex flex-col justify-center items-center rounded-2xl drop-shadow-md text-gray-800 bg-main-bg'>
+    <div className='m-5 p-5 flex flex-col justify-center items-center rounded-2xl drop-shadow-md text-gray-800 bg-main-bg relative'>
         <Header />
 
         <Outlet />
+        <Footer />
 
     </div>
   )
