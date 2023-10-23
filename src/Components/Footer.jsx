4972e76d7e240f248a7ee1783f6b5a59f917bf2a
Kb-Jr <abdulkabiruj@gmail.com>
2023-10-23 19:41:07 +0100

    Update Footer.jsx
    Use a sematic "footer" tag

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='fixed w-full flex items-center justify-center bottom-0 bg-secondary-dark-bg text-white rounded-b-2xl mt-2'>
-            <p className='p-3'>2022 #VanLife</p>
-    </div>
+    <footer className='fixed w-full flex items-center justify-center bottom-0 bg-secondary-dark-bg text-white rounded-b-2xl mt-2'>
+            <p className='p-2'>2022 #VanLife</p>
+    </footer>
   )
 }
 
