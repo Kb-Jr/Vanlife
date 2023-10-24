3622312cc709c66c6a039f4c3e7bfebb401654a6
Kb-Jr <abdulkabiruj@gmail.com>
2023-10-24 11:40:29 +0100

    Update Header.jsx
    Added NavLink style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const Header = () => {
+  const activeLink = 'text-red-500 font-bold underline'
   return (
     <div className='flex w-full justify-around m-4 font-semibold text-2xl'>
-        <Link to='/'>#VANLIFE</Link>
-        <Link to='/hosts'>Hosts</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/vans'>Vans List</Link>
+        <NavLink to='/' className={({isActive}) => isActive ? activeLink : "" }     
+        >#VANLIFE
+        </NavLink>
+        <NavLink to='/hosts' className={({isActive}) => isActive ? activeLink : "" } >
+          Hosts
+        </NavLink>
+        <NavLink to='/about' className={({isActive}) => isActive ? activeLink : "" } >
+          About
+        </NavLink>
+        <NavLink to='/vans' className={({isActive}) => isActive ? activeLink : "" } >
+          Vans List
+        </NavLink>
 
     </div>
   )
